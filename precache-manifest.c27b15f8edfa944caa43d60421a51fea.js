1761a21b1446252276a85c35d8316dbffc081225
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2023-02-11 21:56:00 +0530

    Updates

---
+++ precache-manifest.c27b15f8edfa944caa43d60421a51fea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb662f135d7363aa44ae1d3c0966e70f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "75d53a31449d0415e26b",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "e943e6ea8c0a57b9a0ec",
+    "url": "./static/js/2.41bd069c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.41bd069c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75d53a31449d0415e26b",
+    "url": "./static/js/main.20504b54.chunk.js"
+  },
+  {
+    "revision": "fd4a6e5c794a69f2764d",
+    "url": "./static/js/runtime-main.fa95f09f.js"
+  }
+]);
