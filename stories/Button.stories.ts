895749d310a936b94d139c4cceb70fe415500f06
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2025-05-03 19:41:49 +0530

    feat: setup storybook

---
+++ stories/Button.stories.ts
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Button, { ButtonProps } from "../src/Button";
+
+const meta: Meta<ButtonProps> = {
+  title: "Components/Button",
+  component: Button,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const LargeBlack: Story = {
+  args: {
+    size: "large",
+    background: "black",
+    children: "Large Black"
+  },
+};
+
+export const LargeWhite: Story = {
+  args: {
+    size: "large",
+    background: "white",
+    children: "Large White"
+  },
+};
+
+export const LargeRed: Story = {
+  args: {
+    size: "large",
+    background: "red",
+    children: "Large Red"
+  },
+};
+
+export const LargeGrey: Story = {
+  args: {
+    size: "large",
+    background: "grey",
+    children: "Large Grey"
+  },
+};
