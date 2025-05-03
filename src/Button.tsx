86babca65938c7d0a648e28d86bfccb49e59740d
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2025-05-03 18:18:02 +0530

    feat: add colors

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,7 +1,15 @@
 import "./Button.css";
 
-const Button = () => {
-  return <div className="button">button</div>;
+export interface ButtonProps extends HTMLButtonElement {
+  size: "large" | "small";
+}
+
+const Button = ({ size, ...rest }: ButtonProps) => {
+  return (
+    <button {...rest} className={`button ${size}`}>
+      button
+    </button>
+  );
 };
 
 export default Button;
